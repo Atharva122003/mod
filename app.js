3eaf7db3d85d614e6e69ef068fc06e48d6894dba
Atharva122003 <atharvavj12@gmail.com>
2026-04-15 17:09:31 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+// app.js
+// Import math module and use add & subtract functions
+
+import { add, subtract } from "./math.js";
+
+console.log("Addition:", add(10, 5));
+console.log("Subtraction:", subtract(10, 5));
---
+++ math.js
@@ -0,0 +1,10 @@
+// math.js
+// Export two functions: add and subtract
+
+export function add(a, b) {
+    return a + b;
+}
+
+export function subtract(a, b) {
+    return a - b;
+}
